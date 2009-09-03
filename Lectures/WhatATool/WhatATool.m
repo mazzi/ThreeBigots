5832bd65e94c707bab859367dfd2b013a22b379a
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2009-09-02 23:49:38 -0300

    Assignment1B, parte 2.

--- Lectures/WhatATool/WhatATool.m
+++ Lectures/WhatATool/WhatATool.m
@@ -1,12 +1,13 @@
 #import <Foundation/Foundation.h>
 #import <Foundation/NSPathUtilities.h>
+#import <Foundation/NSProcessInfo.h>
 
-int main (int argc, const char * argv[]) {
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
+/* Section 1: Strings as file system paths */
+void section1() {
+	
 	NSString *path = @"~";
 	path = [path stringByExpandingTildeInPath];  
-
+	
 	NSString *expanded = [@"My home folder is at " stringByAppendingString:path];
 	NSLog(expanded);
 	
@@ -16,8 +17,35 @@ int main (int argc, const char * argv[]) {
 	
 	while(path = [en nextObject]) {
 		NSLog(path);
-	}
+	}	
+}
+
+/* Section 2: Finding out a bit about our own process */
+void section2() {
+	
+	NSProcessInfo *proceso = [NSProcessInfo processInfo];
+
+	NSLog(@"Process Name: '%@' Process ID:'%d'", [proceso processName], [proceso processIdentifier]);	
+}
+
+/* Section 3: A little bookmark dictionary */
+void section3(){
+	
+	// TODO Ver lecture 2 =)
+	
+	//NSMutableDictionary *aDictionary;
 		
+	//[[aDictionary objectForKey:@"Stanford University"] "http://www.stanford.edu"];
+	
+	//[aDictionary removeObjectForKey:theKey];
+	//[anObject someMessage];
+}
+
+int main (int argc, const char * argv[]) {
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	
+	section3();
+	
     [pool drain];
     return 0;
 }
